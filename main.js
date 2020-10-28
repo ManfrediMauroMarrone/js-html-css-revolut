91de1561fbbe5cae20555531613ff2c296cc6f10
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-28 16:54:04 +0100

    aggiunto altri drop down e css

--- main.js
+++ main.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
     $('.drop-function').toggle()
   });
 
+  var menuSubs = $('.right-menu li:nth-child(2) a')
+
+  menuSubs.click(function(){
+    $('.drop-subs').toggle()
+  });
+
+  var menuWho = $('.right-menu li:nth-child(3) a')
+
+  menuWho.click(function(){
+    $('.drop-who').toggle()
+  });
+
+  var menuHelp = $('.right-menu li:nth-child(4) a')
+
+  menuHelp.click(function(){
+    $('.drop-help').toggle()
+  });
+
+
 });
